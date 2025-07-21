f495ddfc80e87964c58277ceb06bde250a1fe147
Banana-Giova <giovannidigiuseppe@protonmail.com>
2025-07-21 14:27:21 +0200

    Inizializzato ramo register, creati il componente, il modulo e il servizio dedicati alla registrazione

---
+++ it-tickets/src/app/pages/register/register.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Register } from './register';
+
+@NgModule({
+  declarations: [
+    Register
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+
+export class RegisterModule {
+}
