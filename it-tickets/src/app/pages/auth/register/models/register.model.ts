79067655b06d1305f5011ab9e026ec70ec601c81
Banana-Giova <giovannidigiuseppe@protonmail.com>
2025-07-28 17:44:10 +0200

    Refactoring nomenclatura

--- it-tickets/src/app/pages/auth/register/models/register.model.ts
+++ it-tickets/src/app/pages/auth/register/models/register.model.ts
@@ -2,7 +2,7 @@ export class RegisterModel {
     name: string = '';
     surname: string = '';
     email: string = '';
-    password: string = '';
+    newPassword: string = '';
     confirmPassword: string = '';
 
     constructor(data?:any) {
@@ -10,8 +10,8 @@ export class RegisterModel {
     }
 
     isValidPassword = () => {
-        if (this.password != this.confirmPassword) { return false };
-        return /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,24}$/.test(this.password);
+        if (this.newPassword != this.confirmPassword) { return false };
+        return /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,24}$/.test(this.newPassword);
     }
 
     isValidEmail = () => {
