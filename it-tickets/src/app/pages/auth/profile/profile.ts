8efe1167e68572ad4a96ef9d9be01fbea7f4acbe
DESKTOP-TIDVE16\utente01 <giovannidigiuseppe@protonmail.com>
2025-09-02 18:06:30 +0200

    Correzione typo

--- it-tickets/src/app/pages/auth/profile/profile.ts
+++ it-tickets/src/app/pages/auth/profile/profile.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ProfileModel } from './models/profile';
+import { ProfileModel } from './models/profile.model';
 import { Router } from '@angular/router';
 
 @Component({
