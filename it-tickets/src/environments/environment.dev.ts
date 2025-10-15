422ed4838ab3f3e925db459833881a8477ce8907
gaiaflati <gaiaflati@live.it>
2025-10-15 11:55:34 +0200

    cambio porta del progetto

--- it-tickets/src/environments/environment.dev.ts
+++ it-tickets/src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environments = {
-    baseUrl: 'http://localhost:8080/',
+    baseUrl: 'http://localhost:9090/',
     register: 'register',
     login: 'login',
         logout: 'auth/logout',
